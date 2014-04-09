03171a8a24c5e39e04ed0043fad93ae0becfbc57
bibidon <debibidon@gmail.com>
2014-04-09 23:06:34 +0300

    Calculator Summi & Error

---
+++ JS/CalSumAndError.js
@@ -0,0 +1,34 @@
+﻿var summa = new Object;
+summa.result = 0;
+summa.errors = [];
+
+function SummaArgs(a) {
+    var j = 0;
+    var s = 0;
+    for (var i in a) {
+        if (j > 9) {
+            throw new Error('Параметров должно быть не более десяти, а вы передали ' + (j + 1));
+        }
+        switch (typeof a[i]) {
+            case 'string':
+                summa.errors.push(j + ' параметр является строкой');
+                j++;
+                continue;
+            case 'object':
+                summa.errors.push(j + ' параметр является объектом');
+                j++;
+                continue;
+            case 'function':
+                summa.errors.push(j + ' параметр является функцией');
+                j++;
+                continue;
+            case 'boolean':
+                summa.errors.push(j + ' параметр является логическим значением');
+                j++;
+                continue;
+        }
+        s += a[i];
+        j++;
+    }
+    summa.result = s;
+}
